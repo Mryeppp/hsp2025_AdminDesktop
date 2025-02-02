ebaf791d22f018d0a0c822cdd9417c2f32d77853
Mryeppp <362105600@qq.com>
2025-02-02 21:30:07 +0800

    2.2

--- src/Chapter13/Scanner_/scanner01_.java
+++ src/Chapter13/Scanner_/scanner01_.java
@@ -1,4 +1,17 @@
 package Chapter13.Scanner_;
 
+import java.util.Scanner;
+
 public class scanner01_ {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//        System.out.println(str);
+//        System.out.println(sc.nextInt());
+        Integer a = 45;
+        System.out.println(a.byteValue());
+        System.out.println(a.hashCode());
+    }
+
+
 }
--- src/Chapter13/Wrapper/Wrapper01.java
+++ src/Chapter13/Wrapper/Wrapper01.java
@@ -2,6 +2,16 @@ package Chapter13.Wrapper;
 
 public class Wrapper01 {
     public static void main(String[] args) {
-        System.out.println("a");
+        Integer a = 127;
+        int b = 127;
+
+        int c = 128;
+        Integer d = 128;
+        //Inteager a = x ; 的底层代码 => Integer a = Integer.ValueOf(x)
+        // x在-128 - 127 之间直接返回数字 相当于int a = x ;
+        // 在其他范围是创建一个新的对象
+
+        System.out.println(a==b); //true
+        System.out.println(c==d); //false
     }
 }
